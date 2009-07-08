39dc7c34f847811326b9931175931b5134080326
David Soergel <dev@davidsoergel.com>
2009-07-08 00:31:57 +0000

    CommunitySFI concurrency bug

--- src/main/java/edu/berkeley/compbio/sequtils/TranslatingSequenceReader.java
+++ src/main/java/edu/berkeley/compbio/sequtils/TranslatingSequenceReader.java
@@ -90,8 +90,8 @@ public abstract class TranslatingSequenceReader implements SequenceReader
 			{
 			translate();
 			}
-		int result = translatedBuf[bufPosition
-		                           - 1];// ** count on bufPosition being incremented by the read call above... yucky
+		// ** count on bufPosition being incremented by the read call above... yucky
+		int result = translatedBuf[bufPosition - 1];
 		if (result == -1)
 			{
 			throw new TranslationException("Symbol not in alphabet: " + buf[bufPosition - 1]);
