8decd943d417c07886992c307e098e105cde3216
David Soergel <dev@davidsoergel.com>
2009-11-19 09:48:30 +0000

    Cleaning up dsutils-trees separation

---
+++ src/test/java/edu/berkeley/compbio/sequtils/strings/IntKcountAbstractTest.java
@@ -0,0 +1,38 @@
+package edu.berkeley.compbio.sequtils.strings;
+
+import com.davidsoergel.dsutils.ContractTest;
+import com.davidsoergel.dsutils.ContractTestAwareContractTest;
+import com.davidsoergel.dsutils.TestInstanceFactory;
+
+import java.util.Queue;
+
+/**
+ * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
+ * @version $Id$
+ */
+public class IntKcountAbstractTest<T extends IntKcount>
+		extends ContractTestAwareContractTest<Kcount>//implements TestInstanceFactory<Kcount>
+	{
+// ------------------------------ FIELDS ------------------------------
+
+	protected final TestInstanceFactory<T> tif;
+
+
+// --------------------------- CONSTRUCTORS ---------------------------
+
+	public IntKcountAbstractTest(final TestInstanceFactory<T> tif)
+		{
+		this.tif = tif;
+		}
+
+// -------------------------- OTHER METHODS --------------------------
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void addContractTestsToQueue(final Queue<ContractTest> theContractTests)
+		{
+		theContractTests.add(new KcountAbstractTest<T>(tif));
+		}
+	}
